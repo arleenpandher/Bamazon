8276cac271393152c9c4ea98fa66669949581c50
arleen <arleenpandher11@gmail.com>
2022-01-23 16:57:46 -0500

    logo

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,10 +3,14 @@ import LoginContainer from "../components/session_form/login_form_container"
 import SignupContainer from "../components/session_form/signup_form_container"
 import { Route, Routes, Switch } from "react-router-dom"
 import NavBar from "../components/nav_bar/nav_bar_container";
+import Logo from "../../app/assets/images/logo.png"
+
 
 const App = () => (
   <div>
-    <h1>Herozon</h1>
+    <div id="formlogo">
+      <img  src={Logo}></img>
+    </div>
     <Switch>
         <Route exact path="/" component={NavBar}/>
         <Route path="/login" component={LoginContainer}/>
