a23f6336be29a752474f322b2e58f279644858db
arleen <arleenpandher11@gmail.com>
2022-02-02 18:44:50 -0500

    Profile Css

--- app/views/api/transactions/index.json.jbuilder
+++ app/views/api/transactions/index.json.jbuilder
@@ -3,6 +3,7 @@
         json.extract! transaction, :id, :user_id, :product_id, :quantity
         product = transaction.product 
         json.extract! product, :title, :description, :cost
+        json.service_id product.service.id 
         json.photoUrl url_for(product.photo)
     end
 end 
--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import NavBar from "../nav_bar/nav_bar_container"
+import {BsPersonCircle} from "react-icons/bs"
+import {Link} from "react-router-dom"
+import {BiShoppingBag} from "react-icons/bi"
 
 class Profile extends React.Component {
     constructor(props) {
@@ -11,17 +14,78 @@ class Profile extends React.Component {
     }
 
     render() {
-        if (!this.props.transactions.length) return null 
+        if (!this.props.transactions.length) return null
+        let total = 0
+        let quantity = 0 
+        this.props.transactions.forEach(transaction => {
+            total += (transaction.quantity * transaction.cost)
+            quantity += transaction.quantity
+        })
         return(
             <div>
                 <NavBar />
-                {this.props.transactions.map(transaction => (
+                <div>
+                    <div id="profileandorderdiv">
+                        <div id="profile">
+                                <BsPersonCircle id="inneruserinfoprofile" size={350}/>
+                                <span id="profilename">Name: {this.props.currentUser.name}</span>
+                                <span id="profilename">Username: {this.props.currentUser.username}</span>
+                        </div>
+                        <div id="profileorders">
+                            <div id="innertopprofilediv">
+                            <div id="toprowofprofile">
+                                <div id="toprowprofilestart">
+                                    <span id="topprofilestarttext">ORDER PLACED</span>
+                                    <span>January 18, 2022</span>
+                                </div>
+                                <div id="toprowprofilestart">
+                                    <span id="topprofilestarttext">TOTAL</span>
+                                    <span>${total}</span>
+                                </div>
+                                <div id="toprowprofilestart">
+                                    <span id="topprofilestarttext">QUANTITY</span>
+                                    <span>{quantity} items</span>
+                                </div>
+                                <div id="toprowprofilestart">
+                                    <span id="topprofilestarttext">SHIP TO</span>
+                                    <span id="topprofileusernametext">{this.props.currentUser.name}</span>
+                                </div>
+                            </div>
+                            <div id="toprowprofileend">
+                                <span id="topprofilestarttext">ORDER #702-5932004-7148203</span>
+                                <span id="topprofileusernametext">View order details</span>
+                            </div>
+                            </div>
+                            <div id="alltransactionsdiv">
+                                {this.props.transactions.map(transaction => (
+                                    <div id="individualtransactionbox">
+                                        <div id="transactionprofilecol">
+                                            <span id="deliveredtransactiontext">Delivered Jan. 22, 2022</span>
+                                            <span id="packagetransactiontxt">Package was handed to {this.props.currentUser.name}</span>
+                                            <div id="transactionprofileprodinfo">
+                                                <img id="transactionimage"src={transaction.photoUrl}/>
+                                                <span id="moretransactiondetails">{transaction.title} - {transaction.description}</span>
+                                            </div>
+                                        </div>
+                                        <div id="transactionbuttons">
+                                            <button id="buyagainbtn"><BiShoppingBag/>&nbsp;&nbsp;&nbsp;&nbsp; Buy it again</button>
+                                            <Link id="buyagainbtn">Write a product review</Link>
+                                            <Link id="buyagainbtn">View your hero</Link>
+                                            <Link id="buyagainbtn">View similar heros</Link>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                    </div>
+                </div>
+                {/* {this.props.transactions.map(transaction => (
                     <div>
                         <span>{transaction.title}</span>
                         <span>{transaction.quantity}</span>
                         <span>{transaction.description}</span>
                     </div>
-                ))}
+                ))} */}
+            </div>
             </div>
         )
     }
